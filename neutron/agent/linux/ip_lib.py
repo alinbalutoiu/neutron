00f7bd26caf4c82eb45d3d7c94b80f4d474390e2
Alin Balutoiu <abalutoiu@cloudbasesolutions.com>
2017-03-06 17:42:56 +0200

    Fixes crash when starting Neutron OVS Agent on Windows
    The neutron OVS Agent does not start on Windows because
    of Linux specific import.
    
    The fix consists in moving the method get_ip_version in
    the common module of ip_lib being the only one used from
    the module. We will import the common module
    agent/common/ip_lib after that.
    
    Change-Id: I95e9a6fa99e0992499f46a4f0debccd47b8a936c
    Closes-Bug: #1670402

--- neutron/agent/linux/ip_lib.py
+++ neutron/agent/linux/ip_lib.py
@@ -1151,6 +1151,10 @@ def add_namespace_to_cmd(cmd, namespace=None):
     return ['ip', 'netns', 'exec', namespace] + cmd if namespace else cmd
 
 
+@removals.remove(
+    message="This will be removed in the future. "
+            "Please use 'neutron.common.utils.get_ip_version' instead.",
+    version='Pike', removal_version='Queens')
 def get_ip_version(ip_or_cidr):
     return common_utils.get_ip_version(ip_or_cidr)
 
